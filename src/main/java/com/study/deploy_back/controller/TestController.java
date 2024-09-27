63b73d5636d7375998b93673470d6500be0fdf02
changs0124 <haeya0124@gmail.com>
2024-09-27 11:42:23 +0900

    deploy

---
+++ src/main/java/com/study/deploy_back/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.study.deploy_back.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${client.address}")
+    private String clientAddress;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> test() {
+        return ResponseEntity.ok(clientAddress + "Test!!");
+    }
+}
